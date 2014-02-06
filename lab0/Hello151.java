34f5f8ce89708a2877671a30044c6d1b317fbb69
Cephalostrum <durandal.rubicon@gmail.com>
2014-02-06 12:36:09 -0500

    Lab 0
    Initial import of cs151 lab work

---
+++ lab0/Hello151.java
@@ -0,0 +1,20 @@
+/** Introduction to Java
+ * 
+ * @author Michael Kemp
+ *
+ */
+public class Hello151 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Mike Kemp");
+	System.out.println("mkemp");
+	System.out.println("3rd Year/Biology");
+	System.out.println("The instructors!");
+	
+    }
+
+}
