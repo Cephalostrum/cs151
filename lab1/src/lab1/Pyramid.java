c233cc218fb0cac7c38a03933ddca5934e7acde6
Cephalostrum <durandal.rubicon@gmail.com>
2014-02-10 14:29:31 -0500

    Pyramids.java commit

---
+++ lab1/src/lab1/Pyramid.java
@@ -0,0 +1,33 @@
+package lab1;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		String space = " ";
+		String asterisk = "*";
+		
+		if (args.length == 1) {
+			try {
+		        int height = Integer.parseInt(args[0]);
+		    } catch (NumberFormatException e) {
+		        System.err.println("Argument" + " must be an integer");
+		        System.exit(1);
+			for (int i = 0, i < n, i++);
+				System.out.println();
+					
+		}
+		
+		if (args.length == 0) {
+			System.out.println("Please include an integer argument!");
+			System.exit(1);
+			
+		} if (args.length > 1){
+			System.out.println("You've supplied too many arguments! Please only use 1.");
+			System.exit(1);
+			
+		
+		}
+		
+	}
+
+}
